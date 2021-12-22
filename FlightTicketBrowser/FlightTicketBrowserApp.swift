aa31fea111d8e50fae5b24dd6fd56e31fe8744ba
Takuya Aso <milanista224@gmail.com>
2021-12-22 22:37:11 +0900

    Initial Commit

---
+++ FlightTicketBrowser/FlightTicketBrowserApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlightTicketBrowserApp.swift
+//  FlightTicketBrowser
+//
+//  Created by Takuya Aso on 2021/12/22.
+//
+
+import SwiftUI
+
+@main
+struct FlightTicketBrowserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
